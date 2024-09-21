14b4094a0bb010657a5f41229ebff5c32f833238
thisioliverthings <Abodeking19999@gmail.com>
2024-09-21 10:34:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,14 +35,17 @@ class TraderBot:
     def suggestion(self, update: Update, context: CallbackContext) -> None:
         user_id = update.message.from_user.id
         suggestion_text = ' '.join(context.args)
-      try:
-        if suggestion_text:
-            context.bot.send_message(chat_id=OWNER_CHAT_ID, text=f"اقتراح من المستخدم {user_id}: {suggestion_text}")
-            update.message.reply_text("✅ تم إرسال اقتراحك بنجاح.")
-        else:
-            update.message.reply_text("❌ يرجى كتابة اقتراحك بعد الأمر.")
+
+        try:
+             if suggestion_text:
+                 context.bot.send_message    (chat_id=7161132306, text=f"اقتراح من المستخدم {user_id}: {suggestion_text}")
+                 update.message.reply_text("✅ تم إرسال اقتراحك بنجاح.")
+             else:
+                  update.message.reply_text("❌ يرجى كتابة اقتراحك بعد الأمر.")
         except Exception as e:
-          logger.Error(f"Error processing suggestion : {e}"
+            logger.error(f"Error handling suggestion: {e}")
+            update.message.reply_text("❌ حدث خطأ أثناء معالجة الاقتراح. يرجى المحاولة لاحقًا.")
+        
     def help_command(self, update: Update, context: CallbackContext) -> None:
         keyboard = [
             [InlineKeyboardButton("📜 الأوامر الأساسية", callback_data='help_section_1')],
