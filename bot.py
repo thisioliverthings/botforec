203d5f7672456dab5a6f9454f705184718fc860c
thisioliverthings <Abodeking19999@gmail.com>
2024-09-21 07:41:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,32 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from database import init_db
-from handlers import handle_commands
-from handlers import correct_command
+from handlers import handle_commands, correct_command
 import json
 import difflib
+
 API_TOKEN = "8119443898:AAFwm5E368v-Ov-M_XGBQYCJxj1vMDQbv-0" 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def start_bot():
+    updater = Updater(API_TOKEN, use_context=True)
     
-    updater = Updater("TOKEN", use_ontext=True)
     # معالجة الأوامر المعروفة
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', handle_message))
-    dispatcher.add_handler(CommandHandler('help', handle_message))
-    dispatcher.add_handler(CommandHandler('حسابي', handle_message))
-    dispatcher.add_handler(CommandHandler('اقتراح', handle_message))
-    dispatcher.add_handler(CommandHandler('سحب', handle_message))
-    dispatcher.add_handler(CommandHandler('إيداع', handle_message))
+    dispatcher.add_handler(CommandHandler('start', handle_commands))
+    dispatcher.add_handler(CommandHandler('help', handle_commands))
+    dispatcher.add_handler(CommandHandler('حسابي', handle_commands))
+    dispatcher.add_handler(CommandHandler('اقتراح', handle_commands))
+    dispatcher.add_handler(CommandHandler('سحب', handle_commands))
+    dispatcher.add_handler(CommandHandler('إيداع', handle_commands))
 
     # تصحيح الأخطاء الشائعة
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, correct_command))
 
-Updater.start_polling()
-Updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     init_db()
