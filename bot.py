ae8a78cad5f0e7694385ac2837a83a40d73e414e
thisioliverthings <Abodeking19999@gmail.com>
2024-09-21 07:44:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,37 @@
 import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from database import init_db
+from handlers import handle_commands
+import json
+# إعداد الـ Token الخاص بالبوت
+API_TOKEN = '8119443898:AAFwm5E368v-Ov-M_XGBQYCJxj1vMDQbv-0'
+
+# تفعيل نظام التسجيل لمراقبة الأخطاء
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def start_bot():
+    updater = Updater(token=API_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', handle_commands))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_commands))
+
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    init_db()
+    start_bot()
+    
+    
+    
+    
+    
+    
+    import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from database import init_db
 from handlers import handle_commands, correct_command
@@ -17,10 +50,10 @@ def start_bot():
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', handle_commands))
     dispatcher.add_handler(CommandHandler('help', handle_commands))
-dispatcher.add_handler(CommandHandler('/حسابي', handle_commands))
-dispatcher.add_handler(CommandHandler('/اقتراح', handle_commands))
-dispatcher.add_handler(CommandHandler('/سحب', handle_commands))
-dispatcher.add_handler(CommandHandler('/إيداع', handle_commands))
+    dispatcher.add_handler(CommandHandler('/حسابي', handle_commands))
+    dispatcher.add_handler(CommandHandler('/اقتراح', handle_commands))
+    dispatcher.add_handler(CommandHandler('/سحب', handle_commands))
+    dispatcher.add_handler(CommandHandler('/إيداع', handle_commands))
     # تصحيح الأخطاء الشائعة
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, correct_command))
 
