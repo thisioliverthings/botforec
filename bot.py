72c2a82fb5f215dc86c21095b0d8cb2e23208cd6
thisioliverthings <Abodeking19999@gmail.com>
2024-09-21 06:59:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler, Updater
+from database import load_user_data, save_user_data
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-from database import init_db
-from handlers import handle_commands,button
 import json
+
 # إعداد الـ Token الخاص بالبوت
 API_TOKEN = '8119443898:AAFwm5E368v-Ov-M_XGBQYCJxj1vMDQbv-0'
 
@@ -11,14 +12,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def start_bot():
-    updater = Updater(token=API_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler('start', handle_commands))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_commands))
-
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    
+    dispatcher.add_handler(CommandHandler('start', handle_message))
+    dispatcher.add_handler(CommandHandler('help', help_command))
+    dispatcher.add_handler(CommandHandler('حسابي', handle_account_info))
+    dispatcher.add_handler(CommandHandler('اقتراح', suggestion))
+    dispatcher.add_handler(CommandHandler('سحب', handle_withdraw))
+    dispatcher.add_handler(CommandHandler('إيداع', handle_deposit))
     updater.start_polling()
     updater.idle()
 
