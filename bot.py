b0a8681f22f408a868a769ee8bcc276982584ece
thisioliverthings <Abodeking19999@gmail.com>
2024-09-21 08:32:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,30 @@
+import json
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from database import init_db
 from handlers import handle_commands, correct_command, button
-import json
- 
+
+# إعداد الـ Token الخاص بالبوت
+API_TOKEN = 'YOUR_API_TOKEN_HERE'
+
+# تفعيل نظام التسجيل لمراقبة الأخطاء
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def start_bot():
+    updater = Updater(token=API_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', handle_commands))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_commands))
+    dispatcher.add_handler(CallbackQueryHandler(button))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    init_db()
+    start_bot()
 # إعداد الـ Token الخاص بالبوت
 API_TOKEN = '8119443898:AAFwm5E368v-Ov-M_XGBQYCJxj1vMDQbv-0'
 
