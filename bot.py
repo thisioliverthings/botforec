68a574992b687b211ab5cf1ae3fc979d4d90a8f6
thisioliverthings <Abodeking19999@gmail.com>
2024-09-21 07:57:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from database import init_db
-from handlers import handle_commands
+from handlers import handle_commands, correct_command, button
 import json
-from handlers import correct_command
+ 
 # إعداد الـ Token الخاص بالبوت
 API_TOKEN = '8119443898:AAFwm5E368v-Ov-M_XGBQYCJxj1vMDQbv-0'
 
